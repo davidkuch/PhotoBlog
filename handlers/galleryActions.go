289c01e3d1e1d544b12009d637825db12fd5a68d
david.kuchersky <davidkuchersky@gmail.com>
2022-11-15 18:04:35 +0200

    work on handlers skeleton

---
+++ handlers/galleryActions.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func createGallery(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func addPicToGallery(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func removePicFromGallery(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func annotatePic(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func manageVisibility(res http.ResponseWriter, req *http.Request) {
+
+}
---
+++ handlers/user_home.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func seeDashboard(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func seeGalleries(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func seeFriends(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func seeEvents(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func seeTraffic(res http.ResponseWriter, req *http.Request) {
+
+}
